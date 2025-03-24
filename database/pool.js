18d9ac65b0f9313dfcaea5a6011a244a3e3d3961
daniyal-abbassi <slimshady3753@protonmail.com>
2025-03-25 01:26:25 +0330

    Use DATABASE_URL for Railway deployment

--- database/pool.js
+++ database/pool.js
@@ -1,21 +1,20 @@
 require('dotenv').config();
 const Pool = require('pg').Pool;
 const pool = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_DATABASE
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 })
 
 
-pool.on('error',(error, client)=>{
+pool.on('error', (error, client) => {
     console.error(`Unexpected error on connection: ${error}`)
 })
 
 pool.connect()
-    .then(()=> console.log("Database connected successfully!!!"))
-    .catch(err=>console.error(`Database connection error: ${err}`))
+    .then(() => console.log("Database connected successfully!!!"))
+    .catch(err => console.error(`Database connection error: ${err}`))
 
 
-module.exports=pool;
+module.exports = pool;
